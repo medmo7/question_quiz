2441830b2e2faf7f56a9fe3b058e71fdb83f23e6
Mohamed BOURGA <keyserve.contact@gmail.com>
2022-01-25 09:34:51 +0100

    dev continued on ui

--- src/components/QuestionControl.tsx
+++ src/components/QuestionControl.tsx
@@ -3,27 +3,31 @@ import React from 'react';
 import { COLORS } from '../util/constants';
 
 type Props = {
-    
+
 }
 
 export default function QuestionControl(props: Props) {
 
     const getStatusStyle = () => {
         const style = {
-            containerBg: COLORS.background,
-            buttonBg: COLORS.gray,
-            buttonTitleColor: COLORS.white
+            containerBg: COLORS.success,
+            buttonBg: COLORS.white,
+            buttonTitleColor: COLORS.success
         }
 
         return style
     }
 
     const statusStyle = getStatusStyle()
-
+    const result = "Great Job!"
+    const showResult = true
     return (
-        <View style={{...styles.container, backgroundColor:statusStyle.containerBg}}>
-            <Pressable style={{...styles.button, backgroundColor: statusStyle.buttonBg}}>
-                <Text style={{...styles.buttonTitle, color:statusStyle.buttonTitleColor}}>Continue</Text>
+        <View style={{ ...styles.container, backgroundColor: statusStyle.containerBg }}>
+            {showResult && <View style={styles.resultContainer}>
+                <Text style={styles.result}>{result}</Text>
+            </View>}
+            <Pressable style={{ ...styles.button, backgroundColor: statusStyle.buttonBg }}>
+                <Text style={{ ...styles.buttonTitle, color: statusStyle.buttonTitleColor }}>Continue</Text>
             </Pressable>
         </View>
     );
@@ -32,23 +36,32 @@ export default function QuestionControl(props: Props) {
 const styles = StyleSheet.create({
     container: {
         paddingVertical: 20,
-        borderTopRightRadius:15,
+        borderTopRightRadius: 15,
         borderTopLeftRadius: 15,
         backgroundColor: COLORS.success,
-        justifyContent:'center',
-        alignItems:'center'
+        justifyContent: 'flex-start',
+        alignItems: 'center'
     },
     button: {
-        width:'80%',
+        width: '80%',
         paddingVertical: 15,
-        paddingHorizontal:20,
+        paddingHorizontal: 20,
         backgroundColor: COLORS.gray,
-        borderRadius:25,
-        justifyContent:'center',
-        alignItems:'center'
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonTitle:{
-        fontWeight:'bold',
-        color:COLORS.white
-    }
+    buttonTitle: {
+        fontWeight: 'bold',
+        color: COLORS.white
+    },
+    resultContainer: {
+        padding: 10,
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    result: {
+        color: COLORS.white,
+        fontWeight:'bold'
+    },
 });
